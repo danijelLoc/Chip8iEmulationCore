505020545c55d2e5da404e6ef65fa387e139029c
Danijel Stracenski <danijels222@gmail.com>
2024-05-28 16:26:57 +0200

    Initial Commit

---
+++ Chip8iEmulator/Chip8iEmulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chip8iEmulatorApp.swift
+//  Chip8iEmulator
+//
+//  Created by Danijel Stracenski on 28.05.2024..
+//
+
+import SwiftUI
+
+@main
+struct Chip8iEmulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chip8iEmulator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chip8iEmulator
+//
+//  Created by Danijel Stracenski on 28.05.2024..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
